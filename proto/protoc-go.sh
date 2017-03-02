82f1d7a9f8f2b3bb2ef08d81f4be80575840beb9
Pham Kieu Thanh <thanhpk@live.com>
2017-03-02 11:23:37 +0700

    add compile script

---
+++ proto/protoc-go.sh
@@ -0,0 +1,16 @@
+TOTAL=0
+for i in `ls -R`; do
+  if [[ $i == *":"* ]]; then
+    LASTDIRECTORY=${i::-1}
+  else
+    if [[ $i == *".proto" ]]; then
+      echo -e "\033[0;32mcompilng" $LASTDIRECTORY/$i "\033[0;30m"
+      protoc --go_out=plugins:. --proto_path=../../../../ --proto_path=./  $LASTDIRECTORY/$i
+      let "TOTAL += 1"
+    else
+      echo -e "\033[0;37mignore" $i "\033[0;30m"
+    fi;
+  fi;
+done;
+echo -e "\033[0;34mDone. (total:" $TOTAL "files)\033[0;30m"
+
