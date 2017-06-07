76d95c8d9db704bf571b4e73c0795853352e91a2
Pham Kieu Thanh <thanhpk@live.com>
2017-06-07 18:18:34 +0700

    fix proto build script

--- README.md
+++ README.md
@@ -38,3 +38,20 @@ DashboardUI                                                           ++ ++ -- -
 
   Billing ?
 ```
+
+
+# Rest api
+1. clone swagger-ui
+2. access http://src/swagger-ui/dist/index.html
+3. paste http://src/servicespec/rest/account.yaml to input box
+
+# How to build
+## build proto
+1. cd /proto
+2. ./protoc-go.sh
+## build rest
+1. npm run rest
+
+## build all
+1. ./build.sh
+
--- proto/protoc-go.sh
+++ proto/protoc-go.sh
@@ -8,6 +8,7 @@ for i in `ls -R`; do
     if [[ $i == *".proto" ]]; then
       echo -e "\033[0;37mcompiling" $LASTDIRECTORY/$i "\033[0;31m"
       protoc --go_out=plugins:. --proto_path=../../../../ --proto_path=./  $LASTDIRECTORY/$i
+			protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis --swagger_out=logtostderr=true:. --proto_path=../../../../ --proto_path=./ $LASTDIRECTORY/$i
       let "TOTAL += 1"
     # else
      # echo -e "\033[0;37mignore" $i "\033[0;30m"
