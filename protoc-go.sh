42133d5b34086f7302b86afa8978d1b67dd5e2aa
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2017-10-31 14:28:45 +0700

    include protobuf binary

--- protoc-go.sh
+++ protoc-go.sh
@@ -1,6 +1,8 @@
 # setup
 #go get github.com/favadi/protoc-go-inject-tag
 
+tar xzvf protobuf/bin/protoc.tar.gz
+
 TOTAL=1
 echo -e "\033[0;34mcompiling proto files"
 
@@ -8,14 +10,14 @@ for i in `ls -R`; do
   if [[ $i == *":"* ]]; then
     LASTDIRECTORY=${i::-1}
   else
-		if [[ $i == "vendor" ]] || [[ $LASTDIRECTORY == ./vendor* ]]; then
+		if [[ $i == "vendor" ]] || [[ $i == "proto" ]] || [[ $LASTDIRECTORY == ./vendor* ]] || [[ $LASTDIRECTORY == ./proto* ]]; then
 			continue
 		fi
     if [[ $i == *".proto" ]]; then
       echo -e "\033[0;90m["$TOTAL"] compiling" $LASTDIRECTORY /$i "\033[0;31m"
-      protoc --go_out=plugins:. --proto_path=../../../ --proto_path=./  $LASTDIRECTORY/$i
-			protoc -I/usr/local/include -I. -I$GOPATH/src --swagger_out=logtostderr=true:. --proto_path=../../../ --proto_path=./ $LASTDIRECTORY/$i
-			protoc --python_out=plugins:. --proto_path=../../../ --proto_path=./ $LASTDIRECTORY/$i
+      ./protoc --go_out=plugins:. --proto_path=../../../ --proto_path=./  $LASTDIRECTORY/$i
+			#./protoc -I./protobuf/include -I. -I$GOPATH/src --swagger_out=logtostderr=true:. --proto_path=../../../ --proto_path=./ $LASTDIRECTORY/$i
+			#./protoc --python_out=plugins:. --proto_path=../../../ --proto_path=./ $LASTDIRECTORY/$i
       let "TOTAL += 1"
     # else
      # echo -e "\033[0;37mignore" $i "\033[0;30m"
@@ -28,4 +30,5 @@ let "TOTAL -= 1"
 
 #echo "\033[0;34minjecting golang custom tag\033[0;31m"
 #ls */*.pb.go | xargs -n1 -IX bash -c 'protoc-go-inject-tag -input=X'
+rm protoc
 echo -e "\033[0;32mDone. (total:" $TOTAL "files)\033[0;30m"
