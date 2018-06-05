f2f0b4ff2b7e94b01c04ce0ca7aaac2341792352
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-06-05 16:03:54 +0700

    add scala compiler

--- protoc-go.sh
+++ protoc-go.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+if [ -f bin ]; then
+    echo "should not contains any file name: bin, lib, protoc"
+		exit
+fi
+
+if [ -d bin ]; then
+    echo "should not contains any file name: bin, lib, protoc"
+		exit
+fi
+
+
+if [ -f lib ]; then
+    echo "should not contains any file name: bin, lib, protoc"
+		exit
+fi
+
+
+if [ -d lib ]; then
+    echo "should not contains any file name: bin, lib, protoc"
+		exit
+fi
+
+
 # setup for the first time
 #go get github.com/favadi/protoc-go-inject-tag
 #go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-tar xvf protobuf/bin/protoc.tar.gz
+tar xvf protobuf/bin/protoc2.tar.gz
 
 if [ ! -f $GOPATH/bin/protoc-gen-go ]; then
 		echo -e "\033[0;34minstalling protoc-gen-go..."
@@ -41,11 +64,12 @@ for i in `ls -R`; do
 						./protoc --go_out=plugins:. --proto_path=$GOPATH/src --proto_path=./  $LASTDIRECTORY/$i
 						./protoc -I./protobuf/include -I. -I$GOPATH/src --swagger_out=logtostderr=true:. --proto_path=$GOPATH/src --proto_path=./ $LASTDIRECTORY/$i
 						#./protoc --python_out=plugins:. --proto_path=$GOPATH/src --proto_path=./ $LASTDIRECTORY/$i
-						./protoc -I=$GOPATH/src --java_out=. $GOPATH/src/bitbucket.org/subiz/header/$LASTDIRECTORY/$i
+						# ./protoc -I=$GOPATH/src --java_out=. $GOPATH/src/bitbucket.org/subiz/header/$LASTDIRECTORY/$i
 						ALLPROTO="$ALLPROTO $LASTDIRECTORY/$i"
 						let "TOTAL += 1"
 						# else
 						# echo -e "\033[0;37mignore" $i "\033[0;30m"
+						./bin/scalapbc -v351 -I $GOPATH/src --scala_out=java_conversions:../ $GOPATH/src/bitbucket.org/subiz/header/$LASTDIRECTORY/$i
 				fi;
 		fi;
 done;
@@ -60,4 +84,7 @@ npx pbts --no-comments -o subiz.d.ts subiz.js
 #echo "\033[0;34minjecting golang custom tag\033[0;31m"
 #ls */*.pb.go | xargs -n1 -IX bash -c 'protoc-go-inject-tag -input=X'
 rm protoc
+rm bin -R
+rm lib -R
+
 echo -e "\033[0;32mDone. (total:" $TOTAL "files)\033[0;30m"
