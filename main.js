c49c5a5641ca43f93e683403311420a6311c72c0
Thanh Pham Kieu <thanhpk@live.com>
2019-06-06 12:05:13 +0700

    remove auth

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var root = new pb.Root()
 root.resolvePath = (org, tar) =>
 	pb.util.path.resolve(tar.startsWith('github.com') ? '../../../' : org, tar)
 
-let FILE = './auth/auth.proto'
-let TYPE = 'auth.Permission'
+let FILE = './common.proto'
+let TYPE = 'common.Permission'
 let OBJ = { fullname: 'Thanh', email: 'abc@gmail.com' }
 let BUFFER = new Buffer(
 	'10e00118f601200e306438f00150ff0160f00168f001706e78f0018001ff018801f001a001f001a801f001b001f001b801e001c001e001c80140d001f001d80140e00140f001ff01f801f0018002fe018802f00190020f9802c001a00240a802f001',
