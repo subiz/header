6501b5a1bf7a32da43b22f4f2207d2808c8b8483
Thanh Pham Kieu <thanhpk@live.com>
2021-02-09 01:10:52 +0700

    clean

--- header.go
+++ header.go
@@ -114,11 +114,11 @@ var Scopes = makeScopeMap()
 func makeScopeMap() map[string]string {
 	// scope => permission
 	var m = map[string]string{}
-	m["agent"] = "conversation:rw agent_group:r rule:r integration:r message_template:rw other_message_template:r tag:r whitelist:wr widget:r subscription:r invoice:r user:rw attribute:r facebook:r bot:r agent:r conversation_setting:r web_plugin:r file:wr"
+	m["agent"] = "account:r conversation:rw agent_group:r rule:r integration:r message_template:rw other_message_template:r tag:r whitelist:wr widget:r subscription:r invoice:r user:rw attribute:r facebook:r bot:r agent:r conversation_setting:r web_plugin:r file:wr"
 	m["view_other_convos"] = "other_conversation:r"
 	m["export_user"] = "user:e" // export
 
-	m["account_setting"] = m["agent"] + " agent:w agent_group:w rule:w integration:w other_message_template:rw tag:w widget:w attribute:w facebook:w bot:w conversation_setting:w web_plugin:wr"
+	m["account_setting"] = m["agent"] + "account:w agent:w agent_group:w rule:w integration:w other_message_template:rw tag:w widget:w attribute:w facebook:w bot:w conversation_setting:w web_plugin:wr"
 	m["account_manage"] = "agent_group:wr agent:w subscription:rw payment_method:rw"
 	m["owner"] = m["account_manage"] + " " + m["account_setting"]
 	m["all"] = m["owner"]
