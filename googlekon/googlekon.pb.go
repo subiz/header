6e7246552df5670e96523abdad11ce2b96f2af82
luc <lethienluc2015@gmail.com>
2023-12-18 14:35:23 +0700

    update googlekon

--- googlekon/googlekon.pb.go
+++ googlekon/googlekon.pb.go
@@ -3856,6 +3856,69 @@ func (x *Response) GetTokenType() string {
 	return ""
 }
 
+type GoogleNotiMessage struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Type     *string `protobuf:"bytes,1,opt,name=type" json:"type,omitempty"`
+	Location *string `protobuf:"bytes,2,opt,name=location" json:"location,omitempty"`
+	Review   *string `protobuf:"bytes,3,opt,name=review" json:"review,omitempty"`
+}
+
+func (x *GoogleNotiMessage) Reset() {
+	*x = GoogleNotiMessage{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_googlekon_proto_msgTypes[43]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *GoogleNotiMessage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GoogleNotiMessage) ProtoMessage() {}
+
+func (x *GoogleNotiMessage) ProtoReflect() protoreflect.Message {
+	mi := &file_googlekon_proto_msgTypes[43]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GoogleNotiMessage.ProtoReflect.Descriptor instead.
+func (*GoogleNotiMessage) Descriptor() ([]byte, []int) {
+	return file_googlekon_proto_rawDescGZIP(), []int{43}
+}
+
+func (x *GoogleNotiMessage) GetType() string {
+	if x != nil && x.Type != nil {
+		return *x.Type
+	}
+	return ""
+}
+
+func (x *GoogleNotiMessage) GetLocation() string {
+	if x != nil && x.Location != nil {
+		return *x.Location
+	}
+	return ""
+}
+
+func (x *GoogleNotiMessage) GetReview() string {
+	if x != nil && x.Review != nil {
+		return *x.Review
+	}
+	return ""
+}
+
 var File_googlekon_proto protoreflect.FileDescriptor
 
 var file_googlekon_proto_rawDesc = []byte{
@@ -4439,9 +4502,15 @@ var file_googlekon_proto_rawDesc = []byte{
 	0x73, 0x63, 0x6f, 0x70, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x73, 0x63, 0x6f,
 	0x70, 0x65, 0x12, 0x1d, 0x0a, 0x0a, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x5f, 0x74, 0x79, 0x70, 0x65,
 	0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x54, 0x79, 0x70,
-	0x65, 0x42, 0x23, 0x5a, 0x21, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f,
-	0x73, 0x75, 0x62, 0x69, 0x7a, 0x2f, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x2f, 0x67, 0x6f, 0x6f,
-	0x67, 0x6c, 0x65, 0x6b, 0x6f, 0x6e,
+	0x65, 0x22, 0x5b, 0x0a, 0x11, 0x47, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x4e, 0x6f, 0x74, 0x69, 0x4d,
+	0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x74, 0x79, 0x70, 0x65, 0x12, 0x1a, 0x0a, 0x08, 0x6c, 0x6f,
+	0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6c, 0x6f,
+	0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x16, 0x0a, 0x06, 0x72, 0x65, 0x76, 0x69, 0x65, 0x77,
+	0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x72, 0x65, 0x76, 0x69, 0x65, 0x77, 0x42, 0x23,
+	0x5a, 0x21, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x75, 0x62,
+	0x69, 0x7a, 0x2f, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x2f, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65,
+	0x6b, 0x6f, 0x6e,
 }
 
 var (
@@ -4457,7 +4526,7 @@ func file_googlekon_proto_rawDescGZIP() []byte {
 }
 
 var file_googlekon_proto_enumTypes = make([]protoimpl.EnumInfo, 11)
-var file_googlekon_proto_msgTypes = make([]protoimpl.MessageInfo, 43)
+var file_googlekon_proto_msgTypes = make([]protoimpl.MessageInfo, 44)
 var file_googlekon_proto_goTypes = []interface{}{
 	(BussinessProfileAccount_AccountType)(0),       // 0: googlekon.BussinessProfileAccount.AccountType
 	(BussinessProfileAccount_AccountRole)(0),       // 1: googlekon.BussinessProfileAccount.AccountRole
@@ -4513,6 +4582,7 @@ var file_googlekon_proto_goTypes = []interface{}{
 	(*BatchGetReviews)(nil),                        // 51: googlekon.BatchGetReviews
 	(*LocationReview)(nil),                         // 52: googlekon.LocationReview
 	(*Response)(nil),                               // 53: googlekon.Response
+	(*GoogleNotiMessage)(nil),                      // 54: googlekon.GoogleNotiMessage
 }
 var file_googlekon_proto_depIdxs = []int32{
 	14, // 0: googlekon.BussinessProfileAccount.organization_info:type_name -> googlekon.OrganizationInfo
@@ -5089,6 +5159,18 @@ func file_googlekon_proto_init() {
 				return nil
 			}
 		}
+		file_googlekon_proto_msgTypes[43].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*GoogleNotiMessage); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -5096,7 +5178,7 @@ func file_googlekon_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_googlekon_proto_rawDesc,
 			NumEnums:      11,
-			NumMessages:   43,
+			NumMessages:   44,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
