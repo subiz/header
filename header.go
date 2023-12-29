d917d8dcfeb557231ec52b426905126f76c5b979
Thanh Pham Kieu <thanhpk@live.com>
2023-12-29 16:48:36 +0700

    add lastest rule assign

--- header.go
+++ header.go
@@ -1099,15 +1099,6 @@ func E400(err error, code E, v ...interface{}) error {
 	return log.Error(err, field, log.E_invalid_input, log.E(code.String()))
 }
 
-func E500(err error, code E, v ...interface{}) error {
-	field := log.M{}
-	for i, vv := range v {
-		field[strconv.Itoa(i)] = vv
-	}
-
-	return log.Error(err, field, log.E_internal, log.E(code.String()))
-}
-
 func GetUserType(u *User) string {
 	if u == nil {
 		return ""
