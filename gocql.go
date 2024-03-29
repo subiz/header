59ca31c74c8ce2096216841ffa04afd7422458fc
Thanh Pham Kieu <thanhpk@live.com>
2024-03-30 02:13:29 +0700

    fix gocql

--- gocql.go
+++ gocql.go
@@ -3,7 +3,6 @@ package header
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -11,34 +10,26 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-var hostname string
-
-func init() {
-	hostname, _ = os.Hostname()
-}
-
 var attempCount = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "gocql_count",
 	Help: "The total number of query attempts",
 }, []string{
-	"hostname",
 	"statement",
 })
 
 var attempErrorCount = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "myapp_gocql_attempt_error_count",
+	Name: "gocql_error_count",
 	Help: "The total number of query attempts that failed",
 }, []string{
-	"hostname",
 	"statement",
 })
 
 type queryObserver struct{}
 
 func (qo *queryObserver) ObserveQuery(ctx context.Context, q gocql.ObservedQuery) {
-	attempCount.WithLabelValues(hostname, q.Statement).Inc()
+	attempCount.WithLabelValues(q.Statement).Inc()
 	if q.Err != nil {
-		attempErrorCount.WithLabelValues(hostname, q.Statement).Inc()
+		attempErrorCount.WithLabelValues(q.Statement).Inc()
 	}
 }
 
