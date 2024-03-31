fc8156237c764c6441657797a701fe55d3a51905
Thanh Pham Kieu <thanhpk@live.com>
2024-03-31 22:47:49 +0700

    add gocql type

--- gocql.go
+++ gocql.go
@@ -3,6 +3,7 @@ package header
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -11,25 +12,32 @@ import (
 )
 
 var attempCount = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "gocql_count_2",
+	Name: "gocql_count_3",
 	Help: "The total number of query attempts",
 }, []string{
-	"statement",
+	"statement", "type",
 })
 
 var attempErrorCount = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "gocql_error_count",
+	Name: "gocql_error_count_3",
 	Help: "The total number of query attempts that failed",
 }, []string{
-	"statement",
+	"statement", "type",
 })
 
 type queryObserver struct{}
 
 func (qo *queryObserver) ObserveQuery(ctx context.Context, q gocql.ObservedQuery) {
-	attempCount.WithLabelValues(q.Statement).Inc()
+	typ := "select"
+	if strings.HasPrefix(q.Statement, "INSERT") || strings.HasPrefix(q.Statement, "insert") {
+		typ = "insert"
+	} else if strings.HasPrefix(q.Statement, "DELETE") || strings.HasPrefix(q.Statement, "delete") {
+		typ = "delete"
+	}
+
+	attempCount.WithLabelValues(q.Statement, typ).Inc()
 	if q.Err != nil {
-		attempErrorCount.WithLabelValues(q.Statement).Inc()
+		attempErrorCount.WithLabelValues(q.Statement, typ).Inc()
 	}
 }
 
