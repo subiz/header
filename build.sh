ac87a5c873f539b2af057a089aa1141a28df5d94
Thanh Pham Kieu <thanhpk@live.com>
2024-11-05 11:14:27 +0700

    add create order action

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 # docker build -t live360vn/protobuild:2.9 .
+# docker build -t asia-southeast1-docker.pkg.dev/subiz-version-4/subiz/test:1 .
 docker run -it --rm -v `pwd`:/src live360vn/protobuild:2.9
