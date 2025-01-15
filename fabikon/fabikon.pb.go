0b20b9fdeb46041aac12773b01faefedf894ac77
Thanh Pham Kieu <thanhpk@live.com>
2025-01-15 16:27:57 +0700

    refine

--- fabikon/fabikon.pb.go
+++ fabikon/fabikon.pb.go
@@ -5522,6 +5522,93 @@ func (x *FbConversationMessage) GetAttachments() *FbConversationMessageAttachemn
 	return nil
 }
 
+type FacebookAdsFlow struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id                *string    `protobuf:"bytes,5,opt,name=id" json:"id,omitempty"`
+	Name              *string    `protobuf:"bytes,6,opt,name=name" json:"name,omitempty"`
+	WelcomeMessage    *FbMessage `protobuf:"bytes,7,opt,name=welcome_message,json=welcomeMessage" json:"welcome_message,omitempty"`
+	EligiblePlatforms []string   `protobuf:"bytes,8,rep,name=eligible_platforms,json=eligiblePlatforms" json:"eligible_platforms,omitempty"` //  messenger,instagram
+	IsUsedInAd        *bool      `protobuf:"varint,10,opt,name=is_used_in_ad,json=isUsedInAd" json:"is_used_in_ad,omitempty"`                // fb
+	LastUpdateTime    *string    `protobuf:"bytes,11,opt,name=last_update_time,json=lastUpdateTime" json:"last_update_time,omitempty"`       // fb
+}
+
+func (x *FacebookAdsFlow) Reset() {
+	*x = FacebookAdsFlow{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_fabikon_proto_msgTypes[69]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *FacebookAdsFlow) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FacebookAdsFlow) ProtoMessage() {}
+
+func (x *FacebookAdsFlow) ProtoReflect() protoreflect.Message {
+	mi := &file_fabikon_proto_msgTypes[69]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FacebookAdsFlow.ProtoReflect.Descriptor instead.
+func (*FacebookAdsFlow) Descriptor() ([]byte, []int) {
+	return file_fabikon_proto_rawDescGZIP(), []int{69}
+}
+
+func (x *FacebookAdsFlow) GetId() string {
+	if x != nil && x.Id != nil {
+		return *x.Id
+	}
+	return ""
+}
+
+func (x *FacebookAdsFlow) GetName() string {
+	if x != nil && x.Name != nil {
+		return *x.Name
+	}
+	return ""
+}
+
+func (x *FacebookAdsFlow) GetWelcomeMessage() *FbMessage {
+	if x != nil {
+		return x.WelcomeMessage
+	}
+	return nil
+}
+
+func (x *FacebookAdsFlow) GetEligiblePlatforms() []string {
+	if x != nil {
+		return x.EligiblePlatforms
+	}
+	return nil
+}
+
+func (x *FacebookAdsFlow) GetIsUsedInAd() bool {
+	if x != nil && x.IsUsedInAd != nil {
+		return *x.IsUsedInAd
+	}
+	return false
+}
+
+func (x *FacebookAdsFlow) GetLastUpdateTime() string {
+	if x != nil && x.LastUpdateTime != nil {
+		return *x.LastUpdateTime
+	}
+	return ""
+}
+
 var File_fabikon_proto protoreflect.FileDescriptor
 
 var file_fabikon_proto_rawDesc = []byte{
@@ -6300,10 +6387,25 @@ var file_fabikon_proto_rawDesc = []byte{
 	0x73, 0x18, 0x07, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x29, 0x2e, 0x66, 0x61, 0x62, 0x69, 0x6b, 0x6f,
 	0x6e, 0x2e, 0x46, 0x62, 0x43, 0x6f, 0x6e, 0x76, 0x65, 0x72, 0x73, 0x61, 0x74, 0x69, 0x6f, 0x6e,
 	0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x41, 0x74, 0x74, 0x61, 0x63, 0x68, 0x65, 0x6d, 0x6e,
-	0x74, 0x73, 0x52, 0x0b, 0x61, 0x74, 0x74, 0x61, 0x63, 0x68, 0x6d, 0x65, 0x6e, 0x74, 0x73, 0x42,
-	0x21, 0x5a, 0x1f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x75,
-	0x62, 0x69, 0x7a, 0x2f, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x2f, 0x66, 0x61, 0x62, 0x69, 0x6b,
-	0x6f, 0x6e,
+	0x74, 0x73, 0x52, 0x0b, 0x61, 0x74, 0x74, 0x61, 0x63, 0x68, 0x6d, 0x65, 0x6e, 0x74, 0x73, 0x22,
+	0xee, 0x01, 0x0a, 0x0f, 0x46, 0x61, 0x63, 0x65, 0x62, 0x6f, 0x6f, 0x6b, 0x41, 0x64, 0x73, 0x46,
+	0x6c, 0x6f, 0x77, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x05, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x02, 0x69, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x06, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x3b, 0x0a, 0x0f, 0x77, 0x65, 0x6c, 0x63, 0x6f,
+	0x6d, 0x65, 0x5f, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x07, 0x20, 0x01, 0x28, 0x0b,
+	0x32, 0x12, 0x2e, 0x66, 0x61, 0x62, 0x69, 0x6b, 0x6f, 0x6e, 0x2e, 0x46, 0x62, 0x4d, 0x65, 0x73,
+	0x73, 0x61, 0x67, 0x65, 0x52, 0x0e, 0x77, 0x65, 0x6c, 0x63, 0x6f, 0x6d, 0x65, 0x4d, 0x65, 0x73,
+	0x73, 0x61, 0x67, 0x65, 0x12, 0x2d, 0x0a, 0x12, 0x65, 0x6c, 0x69, 0x67, 0x69, 0x62, 0x6c, 0x65,
+	0x5f, 0x70, 0x6c, 0x61, 0x74, 0x66, 0x6f, 0x72, 0x6d, 0x73, 0x18, 0x08, 0x20, 0x03, 0x28, 0x09,
+	0x52, 0x11, 0x65, 0x6c, 0x69, 0x67, 0x69, 0x62, 0x6c, 0x65, 0x50, 0x6c, 0x61, 0x74, 0x66, 0x6f,
+	0x72, 0x6d, 0x73, 0x12, 0x21, 0x0a, 0x0d, 0x69, 0x73, 0x5f, 0x75, 0x73, 0x65, 0x64, 0x5f, 0x69,
+	0x6e, 0x5f, 0x61, 0x64, 0x18, 0x0a, 0x20, 0x01, 0x28, 0x08, 0x52, 0x0a, 0x69, 0x73, 0x55, 0x73,
+	0x65, 0x64, 0x49, 0x6e, 0x41, 0x64, 0x12, 0x28, 0x0a, 0x10, 0x6c, 0x61, 0x73, 0x74, 0x5f, 0x75,
+	0x70, 0x64, 0x61, 0x74, 0x65, 0x5f, 0x74, 0x69, 0x6d, 0x65, 0x18, 0x0b, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x0e, 0x6c, 0x61, 0x73, 0x74, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x54, 0x69, 0x6d, 0x65,
+	0x42, 0x21, 0x5a, 0x1f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73,
+	0x75, 0x62, 0x69, 0x7a, 0x2f, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x2f, 0x66, 0x61, 0x62, 0x69,
+	0x6b, 0x6f, 0x6e,
 }
 
 var (
@@ -6318,7 +6420,7 @@ func file_fabikon_proto_rawDescGZIP() []byte {
 	return file_fabikon_proto_rawDescData
 }
 
-var file_fabikon_proto_msgTypes = make([]protoimpl.MessageInfo, 69)
+var file_fabikon_proto_msgTypes = make([]protoimpl.MessageInfo, 70)
 var file_fabikon_proto_goTypes = []interface{}{
 	(*FacebookDataset)(nil),                  // 0: fabikon.FacebookDataset
 	(*FacebookPage)(nil),                     // 1: fabikon.FacebookPage
@@ -6389,6 +6491,7 @@ var file_fabikon_proto_goTypes = []interface{}{
 	(*FbImageData)(nil),                      // 66: fabikon.FbImageData
 	(*FbConversationMessages)(nil),           // 67: fabikon.FbConversationMessages
 	(*FbConversationMessage)(nil),            // 68: fabikon.FbConversationMessage
+	(*FacebookAdsFlow)(nil),                  // 69: fabikon.FacebookAdsFlow
 }
 var file_fabikon_proto_depIdxs = []int32{
 	47, // 0: fabikon.FacebookPage.instagram_account:type_name -> fabikon.InstagramUser
@@ -6465,11 +6568,12 @@ var file_fabikon_proto_depIdxs = []int32{
 	63, // 71: fabikon.FbConversationMessage.from:type_name -> fabikon.FbProfile
 	62, // 72: fabikon.FbConversationMessage.to:type_name -> fabikon.FbProfileData
 	64, // 73: fabikon.FbConversationMessage.attachments:type_name -> fabikon.FbConversationMessageAttachemnts
-	74, // [74:74] is the sub-list for method output_type
-	74, // [74:74] is the sub-list for method input_type
-	74, // [74:74] is the sub-list for extension type_name
-	74, // [74:74] is the sub-list for extension extendee
-	0,  // [0:74] is the sub-list for field type_name
+	17, // 74: fabikon.FacebookAdsFlow.welcome_message:type_name -> fabikon.FbMessage
+	75, // [75:75] is the sub-list for method output_type
+	75, // [75:75] is the sub-list for method input_type
+	75, // [75:75] is the sub-list for extension type_name
+	75, // [75:75] is the sub-list for extension extendee
+	0,  // [0:75] is the sub-list for field type_name
 }
 
 func init() { file_fabikon_proto_init() }
@@ -7306,6 +7410,18 @@ func file_fabikon_proto_init() {
 				return nil
 			}
 		}
+		file_fabikon_proto_msgTypes[69].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*FacebookAdsFlow); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -7313,7 +7429,7 @@ func file_fabikon_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_fabikon_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   69,
+			NumMessages:   70,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
