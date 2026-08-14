05578e0de608bf0da216023db5b4d2dac2453cd6
Thanh Pham Kieu <thanhpk@live.com>
2026-08-14 18:31:27 +0700

    refine perm

--- header.go
+++ header.go
@@ -704,6 +704,8 @@ type ObjectType string
 
 const TICKET ObjectType = "ticket"
 const TICKET_TYPE ObjectType = "ticket_type"
+const RECORD_TYPE ObjectType = "record_type"
+const RECORD_TYPE ObjectType = "record"
 const TICKET_TEMPLATE ObjectType = "ticket_template"
 const ACCOUNT ObjectType = "account"
 const CONVERSATION ObjectType = "conversation"
